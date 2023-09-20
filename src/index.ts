332fde77c36381ab0d82b6d8b91b2c915b1186ca
liji <hello_zaizi@163.com>
2023-09-20 17:11:14 +0800

    build: 输出部分常用类型定义

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import './types';
-export * from './usecases/arrayUseCase';
-export * from './usecases/entityGeneratorUseCase';
-export * from './usecases/entityReducerUseCase';
-export * from './usecases/entityUseCase';
-export * from './usecases/objectUseCase';
+
+export { entityUseCase } from './usecases/entityUseCase';
+export { entityGeneratorUseCase } from './usecases/entityGeneratorUseCase';
+export { entityReducerUseCase } from './usecases/entityReducerUseCase';
+export { arrayUseCase } from './usecases/arrayUseCase';
+export { objectUseCase } from './usecases/objectUseCase';
+
+export type { Reducer, Reducers, UseCase, EntityReducer, EntityReducers, EntityUseCase } from './types';
+export type { ArrayReducers } from './usecases/arrayUseCase/types';
+export type { ObjectReducers } from './usecases/objectUseCase/types';
