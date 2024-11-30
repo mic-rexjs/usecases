d6048213679e0d14d67633d8b7d81f2a10d3bc50
liji <hello_zaizi@163.com>
2024-11-30 21:47:43 +0800

    feat(createEntity): 开始支持 object 类型的参数

--- src/methods/createEntity/index.ts
+++ src/methods/createEntity/index.ts
@@ -1,5 +1,18 @@
-import { CreateEntityFactory } from './types';
+import { CreateEntity, CreateEntityFactory } from './types';
 
-export const createEntity = <T>(factory: CreateEntityFactory<T>): T => {
-  return factory();
+export const createEntity: CreateEntity = <T>(
+  entity: CreateEntityFactory<T> | Partial<T>,
+  ...restEntities: Partial<T>[]
+): T => {
+  if (typeof entity === 'function') {
+    return entity();
+  }
+
+  const newEntity: Partial<T> = Object.defineProperties({}, Object.getOwnPropertyDescriptors(entity));
+
+  for (const currentEntity of restEntities) {
+    Object.defineProperties(newEntity, Object.getOwnPropertyDescriptors(currentEntity));
+  }
+
+  return newEntity as T;
 };
--- src/methods/createEntity/types.ts
+++ src/methods/createEntity/types.ts
@@ -1,3 +1,8 @@
 export interface CreateEntityFactory<T> {
   (): T;
 }
+
+export interface CreateEntity {
+  <T>(factory: CreateEntityFactory<T>): T;
+  <T extends object>(...entities: Partial<T>[]): T;
+}
