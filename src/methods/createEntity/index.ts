58642f5d95df1bfa77c599ebf31461666c9be6eb
李骥 <hello_zaizi@163.com>
2026-05-18 11:06:02 +0800

    perf(createEntity): 将自动过滤第 2 个以上参数中的 undefined 的属性

--- src/methods/createEntity/index.ts
+++ src/methods/createEntity/index.ts
@@ -11,7 +11,27 @@ export const createEntity: CreateEntity = <T>(
   const newEntity: Partial<T> = Object.defineProperties({}, Object.getOwnPropertyDescriptors(entity));
 
   for (const currentEntity of restEntities) {
-    Object.defineProperties(newEntity, Object.getOwnPropertyDescriptors(currentEntity));
+    const descriptors = Object.getOwnPropertyDescriptors(currentEntity);
+    const filteredDescriptors: Record<string, PropertyDescriptor> = {};
+    const keys = Object.keys(descriptors);
+
+    for (const key of keys) {
+      const descriptor = descriptors[key];
+      const { value, get, set } = descriptor;
+      const isUndefinedValue = typeof value === 'undefined';
+      const hasGet = typeof get === 'function';
+      const hasSet = typeof set === 'function';
+      const hasAccessor = hasGet || hasSet;
+      const isUndefined = isUndefinedValue && !hasAccessor;
+
+      if (isUndefined) {
+        continue;
+      }
+
+      filteredDescriptors[key] = descriptor;
+    }
+
+    Object.defineProperties(newEntity, filteredDescriptors);
   }
 
   return newEntity as T;
