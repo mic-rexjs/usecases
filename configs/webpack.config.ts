9a74e90ea77547aa0d99bf041490b8f59c97a8df
李骥 <hello_zaizi@163.com>
2026-06-08 10:48:02 +0800

    build: 优化打包方案, 使其支持小程序的直接发布使用, 即 es 目录打包为 chrome 67 & ios 14

--- configs/webpack.config.ts
+++ configs/webpack.config.ts
@@ -29,7 +29,7 @@ const initConfig = (): Configuration => {
           test: /\.js$/,
           loader: 'babel-loader',
           options: {
-            extends: path.resolve(configsDir, '.babelrc'),
+            extends: path.resolve(configsDir, 'babelconfig.json'),
           },
           exclude: /node_modules/,
         },
