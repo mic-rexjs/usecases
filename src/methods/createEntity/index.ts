58d73d55a0e504c4a04a06078321dc509d13962d
李骥 <hello_zaizi@163.com>
2026-07-18 16:58:00 +0800

    fix(createEntity): 修复从第 2 个 entity 开始, 属性访问器不能被设置的 bug

--- src/methods/createEntity/index.ts
+++ src/methods/createEntity/index.ts
@@ -23,45 +23,65 @@ export const createEntity: CreateEntity = <T>(
   }
 
   let isChanged = false;
-  const baseDescriptors = Object.getOwnPropertyDescriptors(entity);
-  const newEntity: Partial<T> = Object.defineProperties({}, baseDescriptors);
+  const descriptors = Object.getOwnPropertyDescriptors(entity) as Record<keyof T, PropertyDescriptor>;
 
   for (const entityItem of restEntities) {
     const keys = Reflect.ownKeys(entityItem);
 
     for (const key of keys) {
-      const baseDescriptor = baseDescriptors[key as keyof T] || {};
-      const { get: basePropertyGetter, set: basePropertySetter } = baseDescriptor;
+      const descriptor = descriptors[key as keyof T] || null;
+      const { get: getter, set: setter } = descriptor || {};
 
-      // 如果原属性是访问器
-      if (basePropertyGetter || basePropertySetter) {
+      // 如果原属性是访问器，那么不能被覆盖
+      if (getter || setter) {
         continue;
       }
 
-      const descriptor = Object.getOwnPropertyDescriptor(entityItem, key) as PropertyDescriptor;
-      const { value, get, set } = descriptor;
-      const isUndefinedValue = typeof value === 'undefined';
-      const hasGet = typeof get === 'function';
-      const hasSet = typeof set === 'function';
-      const hasAccessor = hasGet || hasSet;
-      const isUndefined = isUndefinedValue && !hasAccessor;
+      const newDescriptor = Object.getOwnPropertyDescriptor(entityItem, key) as PropertyDescriptor;
+      const { value: newValue, get: newGetter, set: newSetter } = newDescriptor;
+      const isUndefinedValue = typeof newValue === 'undefined';
+      const hasGet = typeof newGetter === 'function';
+      const hasSet = typeof newSetter === 'function';
+      const isAccessor = hasGet || hasSet;
+      const isUndefined = isUndefinedValue && !isAccessor;
 
       // 如果是 `undefined`
       if (isUndefined) {
         continue;
       }
 
-      const { value: basePropertyValue } = baseDescriptor;
+      if (descriptor === null) {
+        isChanged = true;
+        descriptors[key as keyof T] = newDescriptor;
+        continue;
+      }
+
+      const { value } = descriptor;
+
+      /**
+       * 这里不能直接使用 `newValue`，
+       * 如果是访问器， 那么 `newValue` 一定是 `undefined`，
+       * 而且访问器属性也不能覆盖普通值属性。
+       */
+      const targetValue = entityItem[key as keyof T];
 
       // 如果和原属性值一样
-      if (basePropertyValue === value) {
+      if (value === targetValue) {
         continue;
       }
 
       isChanged = true;
-      Object.defineProperty(newEntity, key, descriptor);
+
+      descriptors[key as keyof T] = {
+        ...descriptor,
+        value: targetValue,
+      };
     }
   }
 
-  return (isChanged ? newEntity : entity) as T;
+  if (!isChanged) {
+    return entity as T;
+  }
+
+  return Object.defineProperties({}, descriptors) as T;
 };
